d342277226f10c3959e8556fd1315ac6c357e2a5
saleemjavid36 <saleemjavid360@gmail.com>
2023-01-04 14:22:51 +0530

    this is new changes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,13 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer= false
+  allowNewServer= false;
+  serverCreationStatus='No server was created';
+
  constructor(){
   setTimeout(()=>{
     this.allowNewServer=true
   },2000)
  }
  ngOnInit(){ 
-
+ }
+ onCreateServer(){
+  this.serverCreationStatus='Server was created';
  }
 }
